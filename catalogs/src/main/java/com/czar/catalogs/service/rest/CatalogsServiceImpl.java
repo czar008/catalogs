3104020a2ff4717374829a9c3a5f5538de391750
1405188 <1405188@22HW005294.SOAM.TCS.com>
2017-09-07 17:39:17 -0500

    api de catalogos

--- catalogs/src/main/java/com/czar/catalogs/service/rest/CatalogsServiceImpl.java
+++ catalogs/src/main/java/com/czar/catalogs/service/rest/CatalogsServiceImpl.java
@@ -26,4 +26,67 @@ public class CatalogsServiceImpl {
 		return new ResponseEntity<String>(estados, HttpStatus.OK);
 	}
 	
+	@RequestMapping(value="/metodosPago/", method=RequestMethod.GET)
+	public ResponseEntity<String> getMetodosPago(){
+		String metodosPago = jaxbToJsonGeneratorService.writeJson("mx.gob.sat.sitio_internet.cfd.catalogos.CMetodoPago");
+		if(metodosPago.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<String>(metodosPago, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/monedas/", method=RequestMethod.GET)
+	public ResponseEntity<String> getMonedas(){
+		String monedas = jaxbToJsonGeneratorService.writeJson("mx.gob.sat.sitio_internet.cfd.catalogos.CMoneda");
+		if(monedas.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}else {
+			return new ResponseEntity<String>(monedas,HttpStatus.OK);
+		}
+		
+	}
+
+	
+	@RequestMapping(value="/paises/", method=RequestMethod.GET)
+	public ResponseEntity<String> getPaises(){
+		String paises = jaxbToJsonGeneratorService.writeJson("mx.gob.sat.sitio_internet.cfd.catalogos.CPais");
+		if(paises.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}else {
+			return new ResponseEntity<String>(paises,HttpStatus.OK);
+		}		
+	}
+	
+	
+	@RequestMapping(value="/tiposComprobante/", method=RequestMethod.GET)
+	public ResponseEntity<String> getTiposComprobante(){
+		String tiposComprobante = jaxbToJsonGeneratorService.writeJson("mx.gob.sat.sitio_internet.cfd.catalogos.CTipoDeComprobante");
+		if(tiposComprobante.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}else {
+			return new ResponseEntity<String>(tiposComprobante,HttpStatus.OK);
+		}		
+	}
+	
+	@RequestMapping(value="/tiposFactor/", method=RequestMethod.GET)
+	public ResponseEntity<String> getTiposFactor(){
+		String tiposFactor = jaxbToJsonGeneratorService.writeJson("mx.gob.sat.sitio_internet.cfd.catalogos.CTipoFactor");
+		if(tiposFactor.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}else {
+			return new ResponseEntity<String>(tiposFactor,HttpStatus.OK);
+		}		
+	}
+	
+	
+	@RequestMapping(value="/usosCFDI/", method=RequestMethod.GET)
+	public ResponseEntity<String> getUsosCFDI(){
+		String usosCFDI = jaxbToJsonGeneratorService.writeJson("mx.gob.sat.sitio_internet.cfd.catalogos.CUsoCFDI");
+		if(usosCFDI.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}else {
+			return new ResponseEntity<String>(usosCFDI,HttpStatus.OK);
+		}		
+	}
+	
 }
